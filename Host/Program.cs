24e82f21e12117fa8862fee4348b84e0a6a6b566
Oliver Booth <oliver@booth.dev>
2025-04-08 13:58:49 +0100

    chore: use serilog

--- Host/Program.cs
+++ Host/Program.cs
@@ -1,9 +1,21 @@
-var builder = WebApplication.CreateBuilder(args);
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.File("logs/latest.log", rollingInterval: RollingInterval.Day)
+#if DEBUG
+    .MinimumLevel.Debug()
+#endif
+    .CreateLogger();
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+builder.Logging.ClearProviders();
+builder.Logging.AddSerilog();
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 
-var app = builder.Build();
+WebApplication app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
